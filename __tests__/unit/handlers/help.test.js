e6e25bfdf407ef56e19b5fea77eeedcf2c279e87
Michael Standen <screaminghawk@gmail.com>
2022-12-16 09:39:26 +1300

    Update help test

--- __tests__/unit/handlers/help.test.js
+++ __tests__/unit/handlers/help.test.js
@@ -1,11 +1,20 @@
 require('dotenv').config()
+const fs = require('fs');
+const path = require('path');
+
 const lambda = require('../../../src/modules/commands/help');
 
-describe('Test for help', function () {
+// Load command list
+const modulesPath = path.join(__dirname, '..', '..', '..', 'src', 'modules')
+const modules = fs.readdirSync(modulesPath, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name)
+const commands = modules.flatMap(m => fs.readdirSync(`${modulesPath}/${m}`))
 
-    it('Verifies successful response', async () => {
+describe('Test for help', function () {
+    it('Verifies each command has a help message', async () => {
         const result = await lambda.action()
         const { embeds } = result
-        expect(embeds[0].fields.length).toEqual(1)
+        expect(embeds[0].fields.length).toEqual(commands.length - 1)
     });
 });
